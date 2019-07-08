62d51d6498f9c51d3188ac1c13f5e8eb199dc01a
wangyao <wangyao@cis.com.cn>
2019-07-08 21:42:24 +0800

    项目初始化

---
+++ src/test/java/com/mohan/project/easytools/EasytoolsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mohan.project.easytools;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasytoolsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
