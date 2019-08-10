0e82c5104dd3815a3a61739bcfc3f4d31631c70a
wangyao <wangyao@cis.com.cn>
2019-08-10 09:42:18 +0800

    JsonFormatter、CharTools

--- src/main/java/com/mohan/project/easytools/common/StringTools.java
+++ src/main/java/com/mohan/project/easytools/common/StringTools.java
@@ -39,9 +39,24 @@ public class StringTools {
     public static final String CR = "\r";
 
     /**
+     * \t
+     */
+    public static final String TAB = "\t";
+
+    /**
+     * ,
+     */
+    public static final String COMMA = ",";
+
+    /**
      * 未找到子串时返回的索引值
      */
     public static final int INDEX_NOT_FOUND = -1;
+
+    /**
+     * <p>The maximum size to which the padding constant(s) can expand.</p>
+     */
+    private static final int PAD_LIMIT = 8192;
 
     /**
      * 是否为空
@@ -300,8 +315,91 @@ public class StringTools {
         return seq.toLowerCase().indexOf(searchStr.toLowerCase());
     }
 
-    public static void main(String[] args) {
-        Optional<String> trim = trim("   S  F  F  S     ");
-        System.out.println(trimAll("ddf eef sadf   ssss     "));
+    /**
+     * <p>Repeat a String {@code repeat} times to form a
+     * new String.</p>
+     *
+     * <pre>
+     * StringUtils.repeat(null, 2) = null
+     * StringUtils.repeat("", 0)   = ""
+     * StringUtils.repeat("", 2)   = ""
+     * StringUtils.repeat("a", 3)  = "aaa"
+     * StringUtils.repeat("ab", 2) = "abab"
+     * StringUtils.repeat("a", -2) = ""
+     * </pre>
+     *
+     * @param str  the String to repeat, may be null
+     * @param repeat  number of times to repeat str, negative treated as zero
+     * @return a new String consisting of the original String repeated,
+     *  {@code null} if null String input
+     */
+    public static String repeat(final String str, final int repeat) {
+        if (str == null) {
+            return null;
+        }
+        if (repeat <= 0) {
+            return EMPTY;
+        }
+        final int inputLength = str.length();
+        if (repeat == 1 || inputLength == 0) {
+            return str;
+        }
+        if (inputLength == 1 && repeat <= PAD_LIMIT) {
+            return repeat(str.charAt(0), repeat);
+        }
+
+        final int outputLength = inputLength * repeat;
+        switch (inputLength) {
+            case 1 :
+                return repeat(str.charAt(0), repeat);
+            case 2 :
+                final char ch0 = str.charAt(0);
+                final char ch1 = str.charAt(1);
+                final char[] output2 = new char[outputLength];
+                for (int i = repeat * 2 - 2; i >= 0; i--, i--) {
+                    output2[i] = ch0;
+                    output2[i + 1] = ch1;
+                }
+                return new String(output2);
+            default :
+                final StringBuilder buf = new StringBuilder(outputLength);
+                for (int i = 0; i < repeat; i++) {
+                    buf.append(str);
+                }
+                return buf.toString();
+        }
+    }
+
+    /**
+     * <p>Returns padding using the specified delimiter repeated
+     * to a given length.</p>
+     *
+     * <pre>
+     * StringUtils.repeat('e', 0)  = ""
+     * StringUtils.repeat('e', 3)  = "eee"
+     * StringUtils.repeat('e', -2) = ""
+     * </pre>
+     *
+     * <p>Note: this method does not support padding with
+     * <a href="http://www.unicode.org/glossary/#supplementary_character">Unicode Supplementary Characters</a>
+     * as they require a pair of {@code char}s to be represented.
+     * If you are needing to support full I18N of your applications
+     * consider using {@link #repeat(String, int)} instead.
+     * </p>
+     *
+     * @param ch  character to repeat
+     * @param repeat  number of times to repeat char, negative treated as zero
+     * @return String with repeated character
+     * @see #repeat(String, int)
+     */
+    public static String repeat(final char ch, final int repeat) {
+        if (repeat <= 0) {
+            return EMPTY;
+        }
+        final char[] buf = new char[repeat];
+        for (int i = repeat - 1; i >= 0; i--) {
+            buf[i] = ch;
+        }
+        return new String(buf);
     }
 }
---
+++ src/main/java/com/mohan/project/easytools/json/JsonFormatter.java
@@ -0,0 +1,49 @@
+package com.mohan.project.easytools.json;
+
+import com.mohan.project.easytools.common.CharTools;
+import com.mohan.project.easytools.common.StringTools;
+
+/**
+ * json格式化器
+ * @author mohan
+ * @date 2019-08-10 09:09:05
+ */
+public class JsonFormatter {
+
+    /**
+     * 将json字符串格式化后输出到控制台
+     * @param json
+     * @return
+     */
+    public static String formateToConsole(String json) {
+        StringBuffer jsonForMatStr = new StringBuffer();
+        int level = 0;
+        int length = json.length();
+        for(int index = 0; index < length; index++) {
+            char c = json.charAt(index);
+            if (level > 0  && CharTools.LF == jsonForMatStr.charAt(jsonForMatStr.length() - 1)) {
+                jsonForMatStr.append(StringTools.repeat(StringTools.TAB, level));
+            }
+            //遇到"{"和"["要增加空格和换行，遇到"}"和"]"要减少空格，以对应，遇到","要换行
+            switch (c) {
+                case CharTools.PREFIX_BIG_BRACKET:
+                case CharTools.PREFIX_MIDDLE_BRACKET:
+                    jsonForMatStr.append(c).append(StringTools.LF);
+                    level++;
+                    break;
+                case CharTools.COMMA:
+                    jsonForMatStr.append(c).append(StringTools.LF);
+                    break;
+                case CharTools.SUFFIX_BIG_BRACKET:
+                case CharTools.SUFFIX_MIDDLE_BRACKET:
+                    jsonForMatStr.append(StringTools.LF);
+                    level--;
+                    jsonForMatStr.append(StringTools.repeat(StringTools.TAB, level)).append(c);
+                    break;
+                default:
+                    jsonForMatStr.append(c);
+            }
+        }
+        return jsonForMatStr.toString();
+    }
+}
