a33aedc3f7ee2ece0052b0a97a0f865e702c9901
wangyao <wangyao@cis.com.cn>
2020-09-17 14:28:30 +0800

    增加首字母转大小写

--- src/main/java/com/mohan/project/easytools/common/StringTools.java
+++ src/main/java/com/mohan/project/easytools/common/StringTools.java
@@ -499,4 +499,30 @@ public final class StringTools {
         }
         return Splitter.on(separator).splitToList(str);
     }
+
+    /**
+     * 首字母转大写
+     * @param s
+     * @return
+     */
+    public static String toUpperCaseFirstOne(String s) {
+        if (Character.isUpperCase(s.charAt(0))) {
+            return s;
+        } else {
+            return (new StringBuilder()).append(Character.toUpperCase(s.charAt(0))).append(s.substring(1)).toString();
+        }
+    }
+
+    /**
+     * 首字母转小写
+     * @param s
+     * @return
+     */
+    public static String toLowerCaseFirstOne(String s) {
+        if (Character.isLowerCase(s.charAt(0))) {
+            return s;
+        } else {
+            return (new StringBuilder()).append(Character.toLowerCase(s.charAt(0))).append(s.substring(1)).toString();
+        }
+    }
 }
